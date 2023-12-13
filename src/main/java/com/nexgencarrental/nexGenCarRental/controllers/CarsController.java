31dfbc4949238cad38992e77eabf73968829f00c
Mustafa Tayyar <mtayyar77@gmail.com>
2023-12-13 20:29:30 +0300

    deneme

--- src/main/java/com/nexgencarrental/nexGenCarRental/controllers/CarsController.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/controllers/CarsController.java
@@ -13,6 +13,8 @@ import java.util.List;
 @RequestMapping("api/cars")
 @AllArgsConstructor
 public class CarsController {
+
+    //
     private final CarService carService;
 
     @GetMapping("/getAll")
