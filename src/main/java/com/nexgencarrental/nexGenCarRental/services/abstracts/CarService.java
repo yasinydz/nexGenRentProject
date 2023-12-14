0e18994aef636a02f59c8c44e81da0b73e0107b4
Hüseyin Demirel <hsyn23dmrl@gmail.com>
2023-12-14 18:14:04 +0300

    Added 'update' as an endpoint in the carManager
    Added 'update' as an endpoint in the carManager

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/CarService.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/CarService.java
@@ -3,12 +3,15 @@ package com.nexgencarrental.nexGenCarRental.services.abstracts;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.car.AddCarRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.car.UpdateCarRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.car.GetCarListResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.car.GetCarResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorListResponse;
 
 import java.util.List;
 
 public interface CarService {
     public List<GetCarListResponse> getAll();
+    public GetCarResponse getById(int id);
+
     public void add(AddCarRequest addCarRequest);
 
     public void update(UpdateCarRequest updateCarRequest);
