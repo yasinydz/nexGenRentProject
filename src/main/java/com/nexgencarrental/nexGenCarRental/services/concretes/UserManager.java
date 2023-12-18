829ebf8d8209c23c72217e91e3f996391e683d19
PickerWork <pickerwork@gmail.com>
2023-12-18 10:36:14 +0300

    User restful api and manager were made.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
@@ -1,4 +1,39 @@
 package com.nexgencarrental.nexGenCarRental.services.concretes;
 
-public class UserManager {
+import com.nexgencarrental.nexGenCarRental.services.abstracts.UserService;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.user.AddUserRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.user.UpdateUserRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.user.GetUserListResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.user.GetUserResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class UserManager implements UserService {
+    @Override
+    public List<GetUserListResponse> getAll() {
+        return null;
+    }
+
+    @Override
+    public GetUserResponse getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(AddUserRequest addUserRequest) {
+
+    }
+
+    @Override
+    public void update(UpdateUserRequest updateUserRequest) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
