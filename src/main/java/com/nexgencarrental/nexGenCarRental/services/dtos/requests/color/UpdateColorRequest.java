ec75f2d099c9db97d2443a5360bfefe082c37769
PickerWork <pickerwork@gmail.com>
2023-12-18 05:38:31 +0300

    Edit UpdateColorRequest.Id field added.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/requests/color/UpdateColorRequest.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/requests/color/UpdateColorRequest.java
@@ -1,5 +1,7 @@
 package com.nexgencarrental.nexGenCarRental.services.dtos.requests.color;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateColorRequest {
+    @Positive(message = "Id alanı 0'dan küçük olamaz.")
+    private int id;
+
+    @Size(min = 2,message = "En az 2 harften oluşan bir renk giriniz" )
     private String name;
 }
