a409f4f2e4e61389745b362fbb6175febed173db
PickerWork <pickerwork@gmail.com>
2023-12-18 08:11:30 +0300

    ModelController api has been designed.

--- src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ModelsController.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ModelsController.java
@@ -3,19 +3,40 @@ package com.nexgencarrental.nexGenCarRental.controllers;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.ModelService;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.car.AddCarRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.model.AddModelRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.model.UpdateModelRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.model.GetModelListResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.model.GetModelResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/models")
 @AllArgsConstructor
 public class ModelsController {
     private final ModelService modelService;
-    @PostMapping
+    @GetMapping("/getAll")
+    public List<GetModelListResponse> getAll(){
+        return modelService.getAll();
+    }
+    @GetMapping("/{id}")
+    public GetModelResponse getModelById(int id){
+        return modelService.getModelById(id);
+    }
+    @PostMapping("/add")
     @ResponseStatus(code = HttpStatus.CREATED)
     public void add(@RequestBody @Valid AddModelRequest addModelRequest) {
         this.modelService.add(addModelRequest);
+    }
+    @PutMapping("/update")
+    public void update(@RequestBody @Valid UpdateModelRequest updateModelRequest){
+        modelService.update(updateModelRequest);
+    }
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+        modelService.delete(id);
     }
 }
