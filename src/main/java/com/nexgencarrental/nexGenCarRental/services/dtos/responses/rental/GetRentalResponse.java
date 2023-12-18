dbbb5d1f678055f5abaf57c15e89152c94f4b95c
PickerWork <pickerwork@gmail.com>
2023-12-18 09:31:47 +0300

    dtos.requests.responses Classes have been edited.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/responses/rental/GetRentalResponse.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/responses/rental/GetRentalResponse.java
@@ -1,4 +1,33 @@
 package com.nexgencarrental.nexGenCarRental.services.dtos.responses.rental;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetRentalResponse {
+    private int id;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private LocalDate returnDate;
+
+    private double startKilometer;
+
+    private double endKilometer;
+
+    private double totalPrice;
+
+    private double discount;
+
+    private int carId;
+
+    private int customerId;
+
+    private int employeeId;
 }
