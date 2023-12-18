b9aaabffa2bfd81460a2bfb215de40ca468db3b7
PickerWork <pickerwork@gmail.com>
2023-12-18 09:42:49 +0300

    UserService business rules defined.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/UserService.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/UserService.java
@@ -1,4 +1,7 @@
 package com.nexgencarrental.nexGenCarRental.services.abstracts;
 
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.user.GetUserResponse;
+
 public interface UserService {
+    GetUserResponse getById(int id);
 }
