87ce4385a458521f1a18d61fe5b958b6068b93ad
PickerWork <pickerwork@gmail.com>
2023-12-18 09:10:13 +0300

    RentalService business rules are written.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/RentalService.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/RentalService.java
@@ -1,4 +1,16 @@
 package com.nexgencarrental.nexGenCarRental.services.abstracts;
 
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.model.AddModelRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.model.UpdateModelRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.rental.GetRentalListResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.rental.GetRentalResponse;
+
+import java.util.List;
+
 public interface RentalService {
+    List<GetRentalListResponse> getAll();
+    GetRentalResponse getById(int id);
+    void add (AddModelRequest addRentalRequest);
+    void update(UpdateModelRequest updateRentalRequest);
+    void delete(int id);
 }
