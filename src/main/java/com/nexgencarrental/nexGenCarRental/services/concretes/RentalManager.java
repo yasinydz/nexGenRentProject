86b6950ff9cb96e9811196d44491d0d498d3aacc
Hüseyin Demirel <hsyn23dmrl@gmail.com>
2023-12-18 17:03:36 +0300

    RentalManager(getadd),CustomerManager(getByid),Employee(getByid) endpoints was written an tested.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/RentalManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/RentalManager.java
@@ -1,6 +1,8 @@
 package com.nexgencarrental.nexGenCarRental.services.concretes;
 
 import com.nexgencarrental.nexGenCarRental.core.utilities.mappers.ModelMapperService;
+import com.nexgencarrental.nexGenCarRental.entities.Car;
+import com.nexgencarrental.nexGenCarRental.entities.Rental;
 import com.nexgencarrental.nexGenCarRental.repositories.RentalRepository;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.CarService;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.CustomerService;
@@ -10,6 +12,10 @@ import com.nexgencarrental.nexGenCarRental.services.dtos.requests.rental.AddRent
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.rental.UpdateRentalRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.car.GetCarListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.car.GetCarResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.customer.GetCustomerResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.employee.GetEmployeeResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.model.GetModelResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.rental.GetRentalListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.rental.GetRentalResponse;
 import lombok.AllArgsConstructor;
@@ -43,6 +49,30 @@ public class RentalManager implements RentalService {
 
     @Override
     public void add(AddRentalRequest addRentalRequest) {
+
+        // Car id kontrolü
+        GetCarResponse getCarResponse = carService.getById(addRentalRequest.getCarId());
+        if (getCarResponse == null) {
+            throw new RuntimeException(addRentalRequest.getCarId() + " id'ye sahip araç sistemde yoktur.");
+        }
+
+        // Customer id kontrolü
+        GetCustomerResponse getCustomerResponse = customerService.getById(addRentalRequest.getCustomerId());
+        if (getCustomerResponse == null) {
+            throw new RuntimeException(addRentalRequest.getCustomerId() + " id'ye sahip müşteri sistemde yoktur.");
+        }
+
+        // Employee id kontrolü
+        GetEmployeeResponse getEmployeeResponse = employeeService.getById(addRentalRequest.getEmployeeId());
+        if (getEmployeeResponse == null) {
+            throw new RuntimeException(addRentalRequest.getEmployeeId() + " id'ye sahip çalışan sistemde yoktur.");
+        }
+
+        // Yeni Sipariş oluşturulması ve kaydedilmesi
+        Rental addRental = modelMapperService.forRequest().map(addRentalRequest, Rental.class);
+
+        //addRental.setId(0);
+        rentalRepository.save(addRental);
 
     }
 
