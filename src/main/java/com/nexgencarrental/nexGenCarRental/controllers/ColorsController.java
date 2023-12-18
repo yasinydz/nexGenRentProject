2984e85d887508b60732e54eba79662effbaa0ca
PickerWork <pickerwork@gmail.com>
2023-12-18 05:50:06 +0300

    Created ColorsController.getAll,update,delete added.

--- src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ColorsController.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ColorsController.java
@@ -3,6 +3,8 @@ package com.nexgencarrental.nexGenCarRental.controllers;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.ColorService;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.car.AddCarRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.AddColorRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.UpdateColorRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,14 +12,20 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/colors")
 @AllArgsConstructor
 public class ColorsController {
     private final ColorService colorService;
-    @GetMapping("{id}")
-    public GetColorResponse getById(int id){
-        return colorService.getById(id);
+    @GetMapping("/getAll")
+    public List<GetColorListResponse> getAll(){
+        return colorService.getAll();
+    }
+    @GetMapping("/{id}")
+    public GetColorResponse getColorById(int id){
+        return colorService.getColorById(id);
     }
 
     @PostMapping("/add")
@@ -25,4 +33,15 @@ public class ColorsController {
     public void add(@RequestBody @Valid AddColorRequest addColorRequest) {
         this.colorService.add(addColorRequest);
     }
+
+    @PutMapping("/update")
+    public void update(@RequestBody @Valid UpdateColorRequest updateColorRequest){
+        colorService.update(updateColorRequest);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+        colorService.delete(id);
+    }
+
 }
