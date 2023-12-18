afc3c91f585dddaee2165dc75b7dae6e6a53e28a
PickerWork <pickerwork@gmail.com>
2023-12-18 10:33:19 +0300

    Employee restful api and manager was made.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/EmployeeManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/EmployeeManager.java
@@ -1,4 +1,40 @@
 package com.nexgencarrental.nexGenCarRental.services.concretes;
 
-public class EmployeeManager {
+import com.nexgencarrental.nexGenCarRental.services.abstracts.EmployeeService;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.employee.AddEmployeeRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.requests.employee.UpdateEmployeeRequest;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.employee.GetEmployeeListResponse;
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.employee.GetEmployeeResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeManager implements EmployeeService {
+    @Override
+    public List<GetEmployeeListResponse> getAll() {
+        return null;
+    }
+
+    @Override
+    public GetEmployeeResponse getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(AddEmployeeRequest addEmployeeRequest) {
+
+    }
+
+    @Override
+    public void update(UpdateEmployeeRequest updateEmployeeRequest) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
