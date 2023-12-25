1b49e6dda9b5001f96aa370ae145127dc267431b
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-12-25 03:11:18 -0200

    UserManager add method created.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
@@ -1,10 +1,16 @@
 package com.nexgencarrental.nexGenCarRental.services.concretes;
 
+import com.nexgencarrental.nexGenCarRental.core.utilities.mappers.ModelMapperService;
+import com.nexgencarrental.nexGenCarRental.entities.Color;
+import com.nexgencarrental.nexGenCarRental.entities.Rental;
+import com.nexgencarrental.nexGenCarRental.entities.User;
+import com.nexgencarrental.nexGenCarRental.repositories.UserRepository;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.UserService;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.user.AddUserRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.user.UpdateUserRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.user.GetUserListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.user.GetUserResponse;
+import com.nexgencarrental.nexGenCarRental.services.rules.user.UserBusinessRulesService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +18,9 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class UserManager implements UserService {
+    private UserRepository userRepository;
+    private UserBusinessRulesService userBusinessRulesService;
+    private ModelMapperService modelMapperService;
     @Override
     public List<GetUserListResponse> getAll() {
         return null;
@@ -24,6 +33,8 @@ public class UserManager implements UserService {
 
     @Override
     public void add(AddUserRequest addUserRequest) {
+        User addUser = modelMapperService.forRequest().map(addUserRequest, User.class);
+        userRepository.save(addUser);
 
     }
 
