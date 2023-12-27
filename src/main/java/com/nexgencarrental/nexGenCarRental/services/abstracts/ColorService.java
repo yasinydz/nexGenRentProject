5746d2f0dd10ea8f644e6adb5b823ba0884063e6
Hüseyin Demirel <hsyn23dmrl@gmail.com>
2023-12-27 21:20:15 +0300

    ColorManager connected to BaseManager & BaseService, tested.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/ColorService.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/abstracts/ColorService.java
@@ -1,21 +1,15 @@
 package com.nexgencarrental.nexGenCarRental.services.abstracts;
 
 import com.nexgencarrental.nexGenCarRental.entities.Color;
-import com.nexgencarrental.nexGenCarRental.entities.Model;
-import com.nexgencarrental.nexGenCarRental.services.dtos.requests.car.AddCarRequest;
+import com.nexgencarrental.nexGenCarRental.repositories.ColorRepository;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.AddColorRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.UpdateColorRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorResponse;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ColorService {
-    public GetColorResponse getColorById(int id);
-    public List<GetColorListResponse> getAll();
-    public void add(AddColorRequest addColorRequest);
-    public void update(UpdateColorRequest updateColorRequest);
-    public void delete(int id);
+public interface ColorService extends BaseService<Color, ColorRepository, GetColorResponse, GetColorListResponse, AddColorRequest, UpdateColorRequest> {
+    void customAdd(AddColorRequest addColorRequest);
+    void customUpdate(UpdateColorRequest updateColorRequest);
 }
--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/ColorManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/ColorManager.java
@@ -1,75 +1,42 @@
 package com.nexgencarrental.nexGenCarRental.services.concretes;
 
 import com.nexgencarrental.nexGenCarRental.core.utilities.mappers.ModelMapperService;
-import com.nexgencarrental.nexGenCarRental.entities.Car;
 import com.nexgencarrental.nexGenCarRental.entities.Color;
-import com.nexgencarrental.nexGenCarRental.entities.Model;
-import com.nexgencarrental.nexGenCarRental.repositories.CarRepository;
+import com.nexgencarrental.nexGenCarRental.entities.Color;
 import com.nexgencarrental.nexGenCarRental.repositories.ColorRepository;
-import com.nexgencarrental.nexGenCarRental.repositories.ModelRepository;
 import com.nexgencarrental.nexGenCarRental.services.abstracts.ColorService;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.AddColorRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.requests.color.UpdateColorRequest;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorListResponse;
 import com.nexgencarrental.nexGenCarRental.services.dtos.responses.color.GetColorResponse;
 import com.nexgencarrental.nexGenCarRental.services.rules.color.ColorBusinessRulesService;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
-public class ColorManager implements ColorService {
-    private final ColorRepository colorRepository;
-    private ModelMapperService modelMapperService;
+public class ColorManager extends BaseManager<Color, ColorRepository, GetColorResponse, GetColorListResponse, AddColorRequest, UpdateColorRequest> implements ColorService {
+    //private final ColorRepository colorRepository;
+    //private ModelMapperService modelMapperService;
     private final ColorBusinessRulesService colorBusinessRulesService;
 
-    @Override
-    public List<GetColorListResponse> getAll() {
-        return colorRepository.findAll().stream()
-                .map(car -> modelMapperService.forResponse()
-                        .map(car, GetColorListResponse.class)).collect(Collectors.toList());
+    public ColorManager(ColorRepository repository, ModelMapperService modelMapperService, ColorBusinessRulesService colorBusinessRulesService) {
+        super(repository, modelMapperService, GetColorResponse.class, GetColorListResponse.class, Color.class, AddColorRequest.class, UpdateColorRequest.class);
+        this.colorBusinessRulesService = colorBusinessRulesService;
     }
 
-    @Override
-    public GetColorResponse getColorById(int id) {
-        Color color = colorRepository.findById(id).orElseThrow(() ->
-                new RuntimeException(id + " girdiğiniz id'ye sahip renk sistemde bulunamıyor."));
-
-        return modelMapperService.forResponse().map(color, GetColorResponse.class);
-    }
 
     @Override
-    public void add(AddColorRequest addColorRequest) {
-
-        // Aynı rengi 2.kez ekleme kontrolü
+    public void customAdd(AddColorRequest addColorRequest) {
         colorBusinessRulesService.existsByName(addColorRequest.getName());
-
-        Color addColor = modelMapperService.forRequest().map(addColorRequest, Color.class);
-
-        colorRepository.save(addColor);
+        add(addColorRequest, Color.class);
     }
 
     @Override
-    public void update(UpdateColorRequest updateColorRequest) {
-
+    public void customUpdate(UpdateColorRequest updateColorRequest) {
         colorBusinessRulesService.existsById(updateColorRequest.getId());
         colorBusinessRulesService.existsByName(updateColorRequest.getName());
-
-        Color color = this.modelMapperService.forRequest()
-                .map(updateColorRequest, Color.class);
-
-        colorRepository.save(color);
+        update(updateColorRequest, Color.class);
     }
-
-    @Override
-    public void delete(int id) {
-        Color deleteColor = colorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Bu id'ye sahip renk bulunamadı."));
-        colorRepository.delete(deleteColor);
-    }
-
 }
